51c0b2695a86664797582ad7f0b3f3aa27072042
Alireza-Mak <info@alirezamak.com>
2023-12-22 21:29:31 -0800

    Add data.txt file and store high score to data.txt

--- main.py
+++ main.py
@@ -9,18 +9,25 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
-
+game_is_on = True
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
+
+
+def end_game():
+    global game_is_on
+    game_is_on = False
+    scoreboard.quit()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
+screen.onkeypress(end_game, "q")
 
-game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
@@ -35,15 +42,15 @@ while game_is_on:
 
     # TODO 5: Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        snake.reset_snake()
+        scoreboard.reset_board()
 
     # TODO 7: Detect collision with food.
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            snake.reset_snake()
+            scoreboard.reset_board()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -11,24 +11,35 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("./data.txt", "r") as data_file:
+            self.high_score = int(data_file.read())
         self.color("white")
         self.penup()
-        self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT1)
+        self.clear()
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}, High Score: {self.high_score}", align=ALIGNMENT, font=FONT1)
+        self.goto(0, -270)
+        self.write('To quit the game press "q"', align=ALIGNMENT, font=FONT3)
 
-    def game_over(self):
+    def quit(self):
         self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT1)
-        self.goto(0, -50)
+        self.color("red")
         self.write("Created by Alireza Mak.", align=ALIGNMENT, font=FONT2)
-        self.goto(0, -100)
+        self.goto(0, -50)
         self.write("Alirezamak.com", align=ALIGNMENT, font=FONT3)
+
+    def reset_board(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("./data.txt", "w") as data_file:
+                data_file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
